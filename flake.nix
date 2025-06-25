5e87a320ed9ab788e7128f64180f08f17cd6f8ef
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-06-25 10:49:02 +0200

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A Nix-flake for the Busy-Bees development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    inputs:
+    let
+      goVersion = 23;
+      nodejsVersion = 24;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+        nodejs = final."nodejs_${toString nodejsVersion}";
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # go (version is specified by overlay)
+              go
+
+              nixfmt-rfc-style
+              nodejs
+
+              # goimports, godoc, etc.
+              gotools
+
+              # https://github.com/golangci/golangci-lint
+              golangci-lint
+            ];
+          };
+        }
+      );
+    };
+}
